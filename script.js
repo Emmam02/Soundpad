536daa3deeea6ccbb1943c03da0f39d573914c61
Emmam02 <135839715+Emmam02@users.noreply.github.com>
2023-08-24 15:06:16 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+function playSound(soundName){ // henter parameter med lydfilen.
+var audio= new Audio(soundName); // lager en ny audio klasse med lydfilen.
+audio.play(); //spiller av lyden som tilhører knappen.
+}
